9a4081001eb61012caa6dd1f442820aff5b6d46c
Pedro Morales Rodriguez <Fyx@MBP-de-Pedro.home>
2015-09-23 11:02:33 +0100

    Set up angular dummy data

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,28 @@
+'use script';
+
 var app = angular.module('notesApp', []);
 
 app.controller('notesController', function($scope) {
-	
+    $scope.categories = [
+        {
+            description: 'Work',
+
+            notes: [
+                {
+                    title: 'This is a title',
+                    content: 'This is some content'
+                }
+            ]
+        },
+        {
+            description: 'Uni',
+
+            notes: [
+                {
+                    title: 'This is a title',
+                    content: 'This is some content'
+                }
+            ]
+        }
+    ];
 });
