39d3d4cd1306a3e9fedac8daaecd3bd07710c45e
Pedro Morales Rodriguez <Fyx@MacBook-Pro-de-Pedro.local>
2015-09-28 11:01:39 +0100

    Added secondary notes bar and animation.

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,21 @@
 var app = angular.module('notesApp', []);
 
 app.controller('notesController', function($scope) {
+
+    $scope.menuItems;
+
     $scope.categories = [
         {
             description: 'Work',
 
             notes: [
                 {
-                    title: 'This is a title',
+                    title: 'This is a title for a note on work',
                     content: 'This is some content'
+                },
+                {
+                    title: 'Chris is stoops',
+                    content: 'I\'m the scatman!'
                 }
             ]
         },
@@ -19,10 +26,26 @@ app.controller('notesController', function($scope) {
 
             notes: [
                 {
-                    title: 'This is a title',
-                    content: 'This is some content'
+                    title: 'This is a title for a note on uni',
+                    content: 'This is some more content about uni'
                 }
             ]
         }
     ];
+
+
+    $scope.selectCat = function(index) {
+        $scope.selectedCat = $scope.categories[index].notes;
+        console.log(index);
+    };
+
+    $scope.switchClass = function(index) {
+        $('.cat_nav').toggleClass('overlay_in');
+    }
+
+    $scope.presentNote = function(index) {
+        $scope.selectedNote = $scope.selectedCat[index];
+        console.log($scope.selectedCat[index]);
+    };
+
 });
