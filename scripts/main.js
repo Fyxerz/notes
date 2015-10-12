45f96f3eab1ff3454044f5e87def1f88645a68d0
Pedro Morales Rodriguez <Fyx@edu09064l340.staff.uce.ac.uk>
2015-10-12 13:50:30 +0100

    Added back button and add note functionality

--- scripts/main.js
+++ scripts/main.js
@@ -2,50 +2,52 @@
 
 var app = angular.module('notesApp', []);
 
-app.controller('notesController', function($scope) {
+app.controller('notesController', function($scope, $http) {
 
-    $scope.menuItems;
+    $scope.selectedNote = {};
+    $scope.categories = {};
 
-    $scope.categories = [
-        {
-            description: 'Work',
-
-            notes: [
-                {
-                    title: 'This is a title for a note on work',
-                    content: 'This is some content'
-                },
-                {
-                    title: 'Chris is stoops',
-                    content: 'I\'m the scatman!'
-                }
-            ]
-        },
-        {
-            description: 'Uni',
-
-            notes: [
-                {
-                    title: 'This is a title for a note on uni',
-                    content: 'This is some more content about uni'
-                }
-            ]
-        }
-    ];
+    $http({
+      method: 'GET',
+      url: '../scripts/cat.json'
+    }).success(function(response) {
+        $scope.categories = response;
+        $scope.selectedCat = $scope.categories[0];
+      }, function errorCallback(response) {
+        console.log('msg');
+      });
 
 
     $scope.selectCat = function(index) {
-        $scope.selectedCat = $scope.categories[index].notes;
-        console.log(index);
+        $scope.selectedCat = $scope.categories[index];
     };
 
-    $scope.switchClass = function(index) {
-        $('.cat_nav').toggleClass('overlay_in');
-    }
+    $scope.presentCat = function() {
+        $('.cat_nav').addClass('overlay_in');
+    };
+
+    $scope.hideCat = function() {
+        $('.cat_nav').removeClass('overlay_in');
+    };
 
     $scope.presentNote = function(index) {
-        $scope.selectedNote = $scope.selectedCat[index];
-        console.log($scope.selectedCat[index]);
+        $scope.selectedNote = $scope.selectedCat.notes[index];
     };
 
+
+    $scope.createNewNote = function() {
+
+        $scope.selectedCat.notes.push(
+            {
+                title: '',
+                content: ''
+            });
+        $scope.selectedNote = $scope.selectedCat.notes[$scope.selectedCat.notes.length - 1];
+        console.log('SelectedCat: ');
+        console.log($scope.selectedCat.notes[$scope.selectedCat.notes.length - 1]);
+        console.log('UniCat: ');
+        console.log($scope.categories[0].notes);
+    };
 });
+
+
